a3a33e8ea435e4897f1dcf109459653763f36633
ilyaryabikin <ilyaryabikin0708@gmail.com>
2020-03-17 14:18:55 +0300

    Fixed output message of GenerateSystemCommand

--- lab1/src/main/java/se/ifmo/cm/cli/command/GenerateSystemCommand.java
+++ lab1/src/main/java/se/ifmo/cm/cli/command/GenerateSystemCommand.java
@@ -6,6 +6,8 @@ import se.ifmo.cm.random.LinearEquationsGenerator;
 import java.util.Scanner;
 
 class GenerateSystemCommand extends Command {
+    private final static int LOWER_BOUND = 2;
+    private final static int HIGHER_BOUND = 20;
 
     protected GenerateSystemCommand(Scanner scanner) {
         super(scanner);
@@ -13,10 +15,10 @@ class GenerateSystemCommand extends Command {
 
     @Override
     protected void execute() throws IllegalArgumentException {
-        System.out.print("Enter variables number (from 0 to 20 inclusively): ");
+        System.out.print("Enter variables number (from " + LOWER_BOUND + " to " + HIGHER_BOUND + " inclusively): ");
         String action = scanner.nextLine();
         int variables = Integer.parseInt(action);
-        if (variables <= 0 || variables > 20) {
+        if (variables < LOWER_BOUND || variables > HIGHER_BOUND) {
             throw new IllegalArgumentException("You entered wrong variables number.");
         }
         dataHolder.setMatrix(LinearEquationsGenerator.generateLinearSystem(variables));
